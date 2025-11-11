acc00574f0bfa76aa89d5ddbb5eb478402497933
rensodalin <rensodalin@user.noreply.github.com>
2025-11-11 22:48:52 +0700

    Update ex4,

--- lib/ex4.dart
+++ lib/ex4.dart
@@ -4,10 +4,6 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFFF5F5F5),
-      ),
       home: Scaffold(
         body: Column(
           children: [
